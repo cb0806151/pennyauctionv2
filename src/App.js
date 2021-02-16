b2d8c26d28a50c7351ef60fc15dfdb78a0c052e4
cb0806151 <cb0806151@otc.edu>
2021-02-15 19:09:42 -0600

    create basic connect wallet button to test functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import * as stdlib from '@reach-sh/stdlib/ETH';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  async connectWallet() {
+    const account = await stdlib.getDefaultAccount();
+    const balanceAtomic = await stdlib.balanceOf(account);
+    const balance = stdlib.formatCurrency(balanceAtomic, 4);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={() => this.connectWallet()}>Connect Wallet</button>
+        </header>
+      </div>
+    )
+  }
+
 }
 
 export default App;
