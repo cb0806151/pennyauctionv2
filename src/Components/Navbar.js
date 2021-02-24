ef2af2988e5186700b1bfcb79d1df57cec34ecb9
cb0806151 <cb0806151@otc.edu>
2021-02-24 15:58:51 -0600

    add component that explains how the game works

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import ConnectWallet from './ConnectWallet'
+import ConnectWallet from './ConnectWallet';
+import Explanation from './Explanation';
+import Button from '@material-ui/core/Button';
 
 export default function Navbar() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     var titleSpace = {
         color: 'white',
@@ -21,8 +32,11 @@ export default function Navbar() {
         <div style={navbar}>
             <div style={titleSpace}>
                 <h1>The Reach Penny Auction</h1>
+                <Button variant="outlined" color="primary" onClick={handleClickOpen}>What's a Penny Auction?</Button>
             </div>
-            <ConnectWallet></ConnectWallet>
+            <ConnectWallet />
+            <Explanation open={open} handleClose={() => handleClose()}></Explanation>
+            
         </div>
     )
 }
